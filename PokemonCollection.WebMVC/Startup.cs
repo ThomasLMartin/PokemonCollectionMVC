956d2ce20d71cfef2293dda0fdef17765701da93
ThomasLMartin <fan.cardinals@gmail.com>
2020-04-14 16:08:08 -0400

    Add project files.

---
+++ PokemonCollection.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokemonCollection.WebMVC.Startup))]
+namespace PokemonCollection.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
